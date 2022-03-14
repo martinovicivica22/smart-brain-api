17afbfda452f0a466dfd21612e68b50f8a18ac13
martinovicivica22 <97768436+martinovicivica22@users.noreply.github.com>
2022-03-14 12:55:33 -0600

    updating db

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-globular-21328',
         user : 'ivica',
         password : '',
         database : 'smart-brain'
